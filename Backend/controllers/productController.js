6483009a4a6494209174652a4fa7e2e9d5a9bc43
Shiv Pratap <sp09singhwaghel@gmail.com>
2023-10-23 20:17:55 +0530

    patch and delete products done

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,30 +1,108 @@
-const productModel=require('../models/productModel');
+const productModel = require("../models/productModel");
 
-const productController=async (req,res)=>{
-    try{
-        const allProducts=await productModel.find({});
+// API TO GET ALL THE PRODUCTS
+const productController = async (req, res) => {
+  try {
+    const allProducts = await productModel.find({});
+    res.status(201).json({
+      allProducts,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// API TO CREATE ALL THE PRODUCTS
+const oneProductController = async (req, res) => {
+  try {
+    const product = await productModel.create(req.body);
+    console.log(product);
+    res.send("Data inserted successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteProductController = async (req, res) => {
+  try {
+    const product = await productModel.findById(req.params.id);
+    console.log(product);
+
+    if (!product) {
+      res.status(400).json({
+        status: false,
+        message: "Data not found",
+      });
+    }
+
+    const foundProduct = await productModel.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Data deleted successfully",
+      status: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      status: false,
+      message: "Data not found",
+      error: { error },
+    });
+  }
+};
+
+// API TO FIND THE PRODUCT
+const findTheProductController = async (req, res) => {
+  const product = await productModel.findById(req.params.id);
+
+  if (!product) {
+    res.status(401).json({
+      status: false,
+      message: "Data not found",
+    });
+  } else {
+    res.status(200).json({
+      status: true,
+      message: "Data found",
+      data: product,
+    });
+  }
+};
+
+// API TO UPDATE THE PRODUCT
+const updateProductController = async (req, res) => {
+  try {
+    const product = await productModel.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    );
+
+    if (!product) {
+      res.status(401).json({
+        status: false,
+        message: "Product not found",
+      });
+    }
+
+    else{
         res.status(201).json({
-            allProducts
+            status:true,
+            message:"Data updated successfully",
         })
     }
-    catch(error){
-        console.log(error);
-    }
-}
+  } 
+  catch (error) {
+    res.status(400).json({
+      status: false,
+      message: "Data not found",
+      error: error,
+    });
+  }
+};
 
-const oneProductController=async (req,res)=>{
-    try{
-        const product=await productModel.create(req.body);
-        console.log(product);
-        res.send("Data inserted successfully");
-        
-    }
-    catch(error){
-        console.log(error)
-    }
-}
-
-module.exports={
-    productController,
-    oneProductController,
-}
+module.exports = {
+  productController,
+  oneProductController,
+  deleteProductController,
+  findTheProductController,
+  updateProductController,
+};
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -2,11 +2,17 @@ const express=require('express');
 
 const router=express.Router();
 
-const {productController, oneProductController}=require("../controllers/productController");
+const {productController, oneProductController, deleteProductController, findTheProductController, updateProductController}=require("../controllers/productController");
 
 router.get('/products',productController);
 
 router.post('/newProduct',oneProductController);
+
+router.delete('/product/:id',deleteProductController);
+
+router.get('/product/:id', findTheProductController);
+
+router.patch('/product/:id', updateProductController)
 
 module.exports=router;
 
